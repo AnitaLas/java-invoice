2719252ec7728b9a7e2046a31efc3a70dd35e649
﻿Liliana Niedzwiedz <liliananiedzwiedz@gmail.com>
2025-02-08 11:27:46 +0100

    change Product

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,47 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Product name cannot be null  or empty");
+        }
+
+        if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price cannot be null or negative");
+        }
+
+        if (tax == null || tax.signum() < 0) {
+            throw new IllegalArgumentException("Tax cannot be null or empty");
+        }
+
+
+     /*//if(name == "" ) {
+     //if(name.isEmpty()) {
+     if(name.isBlank()) {
+            throw new IllegalArgumentException("Product name cannot be empty");
+     }*/
+
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.getPrice().add(this.getPrice().multiply(this.getTaxPercent()));
+        //return getPrice().add(getPrice().multiply(getTaxPercent())); // jak geter się zmieni to, tu też -OCP
+        //return this.price.add(this.taxPercent.multiply(this.price));
     }
+
+
 }
